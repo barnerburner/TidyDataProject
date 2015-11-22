5c074085f29ffdac5aed94a6cf43e5055007657b
Brown Sugar <bladevesuvan@gmail.com>
2015-11-22 17:25:05 -0500

    Final Script

--- run_analysis.R
+++ run_analysis.R
@@ -20,9 +20,36 @@ initdataset <- rbind(testset, trainset)
 ## without forgetting subject and activity columns were appended to front (1, 2, '+2' shift at the end)
 
 features <- read.table("~/UCIHAR/UCI HAR Dataset/features.txt", quote="\"", comment.char="")
-MeansandStds <- sort(c(grep(glob2rx("*mean*"), features[[2]]), grep(glob2rx("*Mean*"), features[[2]]),grep(glob2rx("*std()*"), features[[2]])))
+MeansandStds <- sort(c(grep(glob2rx("*mean*"), features[[2]]), grep(glob2rx("*std()*"), features[[2]])))
 
 paredset <- initdataset[,c(1,2,(MeansandStds+2))]
+
+## Set up vector for column names
+
+varlabels <- c("Subject", "Activity")
+
+for(n in MeansandStds)
+{
+  blip <- as.character(features[n,2])
+  
+  blip <- gsub("Acc", "Accelerometer", blip)
+  blip <- gsub("Gyro", "Gyroscope", blip)
+  blip <- gsub("Mag", "Magnitude", blip)
+  blip <- gsub("BodyBody", "Body", blip)
+  blip <- gsub("-mean()", "Mean", blip)
+  blip <- gsub("-std()", "StandardDeviation", blip)
+  blip <- gsub("Freq", "Frequency", blip)
+  blip <- gsub("X", "X-axis", blip)
+  blip <- gsub("Y", "Y-axis", blip)
+  blip <- gsub("Z", "Z-axis", blip)
+  blip <- gsub("fB", "FB", blip)
+  blip <- gsub("tB", "TB", blip)
+  blip <- gsub("tG", "TG", blip)
+  
+  blip <- paste("Overall", blip, sep = "")
+  
+  varlabels <- c(varlabels, blip)
+}
 
 ## Relabel activities (originally y_sets, now are column 2)
 
@@ -30,14 +57,6 @@ actlabels <- read.table("~/UCIHAR/UCI HAR Dataset/activity_labels.txt", quote="\
 
 for(i in 1:(nrow(paredset)))
   paredset[i, 2] = as.character(actlabels[(paredset[i, 2]),2])
-
-## Set up vector for variable (column) names
-
-features <- read.table("~/UCIHAR/UCI HAR Dataset/features.txt", quote="\"", comment.char="")
-varlabels <- c("Subject", "Activity")
-
-for(n in MeansandStds)
-  varlabels <- c(varlabels, as.character(features[n,2]))
 
 ## Tidy by average for each activity and subject (30 subjects * 6 activities = 180 entries)
 
@@ -49,7 +68,7 @@ condensedset <- cbind(condensedset, sapply(split(paredset[[t]],(paste(as.charact
 
 almostset <- as.data.frame(condensedset)
 
-tidyset <- mutate(almostset, Subject = extract_numeric(rownames(almostset)), Activity = gsub( "^.* ", "", rownames(almostset)))
+tidyset <- mutate(almostset, Subject = extract_numeric(rownames(almostset)), Activity = gsub("^.* ", "", rownames(almostset)))
 col <- ncol(tidyset)
 tidyset <- tidyset[,c(col-1,col,1:(col-2))] %>% arrange(Subject, Activity)
 colnames(tidyset) <- varlabels
